9122c36ddbcf61ffa26f2f3be5975ee3e4ee9139
Brenon-Moraes <brenonmoraes2@gmail.com>
2026-09-24 22:58:30 -0300

    aula do dia 11

---
+++ cardapio-html/assets/js/script.js
@@ -0,0 +1,22 @@
+const categoryLinks = document.querySelectorAll("nav a");
+const selectedCategory = document.querySelector("#selected-category");
+
+categoryLinks.forEach(function (link) {
+	link.addEventListener("click", function (event) {
+		event.preventDefault();
+
+        const categoryName = link.textContent;
+
+        selectedCategory.textContent = "Categoria selecionada: " + categoryName;
+
+     
+
+        categoryLinks.forEach(function (categoryLink) {
+            categoryLink.classList.remove("active");
+		
+    })
+
+    link.classList.add("active");
+		
+	});
+});
